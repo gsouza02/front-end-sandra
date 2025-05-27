04e81b5a35f0d8b8c65b8702f6521071e165944e
Gabriel Souza <souza.gabriel1870@gmail.com>
2025-05-27 09:11:01 -0300

    mudanças

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { CarouselModule } from 'primeng/carousel';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  imports: [HeaderComponent]
+  imports: [HeaderComponent, CarouselModule]
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  images: { previewImageSrc: string; alt: string; title: string }[] = [];
   // Lógica futura para pedidos e catálogo
+
+  ngOnInit() {
+    this.images = [
+      {
+        previewImageSrc: 'assets/images/logo.png',
+        alt: 'Imagem 1',
+        title: 'Imagem 1'
+      }
+      // {
+      //   previewImageSrc: 'assets/images/2.jpg',
+      //   alt: 'Imagem 2',
+      //   title: 'Imagem 2'
+      // },
+      // {
+      //   previewImageSrc: 'assets/images/3.jpg',
+      //   alt: 'Imagem 3',
+      //   title: 'Imagem 3'
+      // }
+    ];
+  }
 }
